fe4df7617a70196102323c5a33e8d5bca384432a
Moisarm <moisarm03@gmail.com>
2025-07-27 01:04:48 -0400

    [Start] server first endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder, web};
+
+#[actix_web::main] //Macro que permite funciones asíncronas
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(
+        /*closure*/ || App::new().route("/", web::get().to(test)),
+    )
+    .bind(("127.0.0.1", 3001))?
+    .run()
+    .await
+}
+
+async fn test() -> impl Responder {
+    "Hallo"
 }
